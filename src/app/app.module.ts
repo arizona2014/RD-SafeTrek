ee3b4e78a14511e6adaf6eaea56b40f2bbe99480
arizona2014 <andy.lisac@gmail.com>
2018-03-20 21:04:09 +0100

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { AuthService } from "./auth.service";
+import { RouterModule, Routes } from "@angular/router";
+import { HttpModule } from "@angular/http";
 
+const appRoutes: Routes = [
+    { path: '', component: AppComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ AuthService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
